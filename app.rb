8baf49c6f3e0c3e4d747db3c6943211e3eb52aed
RubyKobayashi <ruby@rubykobayashi.com>
2019-01-21 16:28:33 +0000

    Sinatra: using forms

--- app.rb
+++ app.rb
@@ -14,11 +14,17 @@ get '/goodbye' do
   "goodbye"
 end
 
-get '/cat' do
-  @random = ["Amigo", "Oscar", "Viking"].sample
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
 erb(:index)
 end
 
 get '/penguin' do
   "penguin"
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
 end
